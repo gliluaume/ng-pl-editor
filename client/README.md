60e05710638843256b07bace38a250e14a8871a1
guillaume <salicisguillaume@yahoo.fr>
2017-03-03 10:28:56 +0100

    fix test failure due to karma configuration

--- client/README.md
+++ client/README.md
@@ -1,1 +1,3 @@
 based on `angular-seed` — the seed for AngularJS apps
+## TODO
+remove angular.ui.sortable because it depends on JQuery.
--- client/karma.conf.js
+++ client/karma.conf.js
@@ -5,11 +5,14 @@ module.exports = function(config) {
     basePath: './app',
 
     files: [
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/jquery-ui/jquery-ui.min.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-bootstrap/ui-bootstrap.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-ui-sortable/sortable.js',
       '**/*.module.js',
       '*!(.module|.spec).js',
       '!(bower_components)/**/*!(.module|.spec).js',
