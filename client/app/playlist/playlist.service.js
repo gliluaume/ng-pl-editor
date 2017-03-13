60df6d3c46de773b2763899e13f63e7a0f561cac
guillaume <salicisguillaume@yahoo.fr>
2017-03-13 11:20:10 +0100

    refacto playlist

--- client/app/playlist/playlist.service.js
+++ client/app/playlist/playlist.service.js
@@ -3,8 +3,8 @@
 angular.module('plEditor.playlist')
 .service('playlistService', ['$filter', function($filter) {
   var plStartTime = 21600;
-  // var plEndTime = 86400;
-  var plEndTime = 25400;
+  var plEndTime = 86400;
+  //var plEndTime = 25400;
   var padLeft = function(text, char, len) {
     text = text.toString();
     char = char.toString();
@@ -47,96 +47,96 @@ angular.module('plEditor.playlist')
     return playlist[playlist.length - 1].startTime + playlist[playlist.length - 1].duration;
   }
 
-  var svc = {};
-  svc.playlist = [];
-  svc.metadata = {};
-  svc.playlistStart = plStartTime;
-  svc.playlistEnd = plEndTime;
+  var svc = {
+    playlist: [],
+    metadata: {},
+    playlistStart: plStartTime,
+    playlistEnd: plEndTime,
 
-  svc.createDaysLocal = function() {
-    var apiDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-    // peuple la liste des jours:
-    var days = [];
-    var d = new Date(1970, 0, 1);
-    d.setDate(d.getDate() - 5);
-    for(var i = 0; i < 7; i++){
-      d.setDate(d.getDate() + 1);
-      days.push({
-        index: i, 
-        label: d.toLocaleString(window.navigator.language, {weekday: 'long'}),
-        apiAlias: apiDays[i],
-        lblNg: $filter('date')(d, 'EEEE')
-      })
-    }
-    console.log('days', days);
-    return days;
+    createDaysLocal: function() {
+      var apiDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+      // peuple la liste des jours:
+      var days = [];
+      var d = new Date(1970, 0, 1);
+      d.setDate(d.getDate() - 5);
+      for(var i = 0; i < 7; i++){
+        d.setDate(d.getDate() + 1);
+        days.push({
+          index: i, 
+          label: d.toLocaleString(window.navigator.language, {weekday: 'long'}),
+          apiAlias: apiDays[i],
+          lblNg: $filter('date')(d, 'EEEE')
+        })
+      }
+      console.log('days', days);
+      return days;
+    },
+
+    buildPlaylist: function(rawPlaylist, tracks) {      
+      var enrichedPlaylist = [];
+      if(rawPlaylist.length > 0) {
+        rawPlaylist.forEach(function(trackId) {
+          var track = tracks.filter(function(track) {
+            return track.id == trackId;
+          }).pop();
+          calculateStartTime(track, enrichedPlaylist);
+          enrichedPlaylist.push(Object.assign({}, track));
+        });
+        enrichedPlaylist = enrichedPlaylist;
+        console.log('enrichedPlaylist', enrichedPlaylist, enrichedPlaylist.length);
+      }
+      svc.playlist = enrichedPlaylist;
+      svc.setMetadata();
+      console.log('svc.metadata', svc.metadata);
+      return svc.playlist;
+    },
+
+    isMaxSizeReached: function(playlist) {
+      return playlist[playlist.length - 1].startTime >= svc.playlistEnd;
+    },
+
+    plRange: function(playlist) {
+      if(playlist.length > 0) {
+        let plEnd = secondsToHours(plSecondEnd(playlist));
+        return `${playlist[0].literalStart} - ${plEnd}`
+      }
+      return '--';
+    },
+
+    availableSpace: function(pl) {
+      let playlist = pl ? pl : svc.playlist;
+      let availableSeconds;
+      if(playlist.length > 0) 
+        availableSeconds = (svc.playlistEnd - svc.playlistStart) - (plSecondEnd(playlist) - playlist[0].startTime);
+      else
+        availableSeconds = (svc.playlistEnd - svc.playlistStart);
+
+      return {
+        seconds: availableSeconds,
+        literal: secondsToHours(availableSeconds)
+      }
+    },
+
+    plRate: function(playlist) {
+      console.log('in svc', playlist);
+      if(playlist.length > 0) {
+        let toto = Math.min(100, Math.round( 100 * (plSecondEnd(playlist) - playlist[0].startTime) / (svc.playlistEnd - svc.playlistStart) ));
+        return toto;
+      }
+      return 0;
+    },
+
+    setMetadata: function() {
+      var plRate = svc.plRate(svc.playlist);
+      svc.metadata.values = { 
+        range : svc.plRange(svc.playlist), 
+        rate : plRate,
+        isMaxSizeReached: plRate >= 100
+      };
+    },
+
+    secondsToHours: secondsToHours
   };
-
-  svc.buildPlaylist = function(rawPlaylist, tracks) {      
-    var enrichedPlaylist = [];
-    if(rawPlaylist.length > 0) {
-      rawPlaylist.forEach(function(trackId) {
-        var track = tracks.filter(function(track) {
-          return track.id == trackId;
-        }).pop();
-        calculateStartTime(track, enrichedPlaylist);
-        enrichedPlaylist.push(Object.assign({}, track));
-      });
-      enrichedPlaylist = enrichedPlaylist;
-      console.log('enrichedPlaylist', enrichedPlaylist, enrichedPlaylist.length);
-    }
-    svc.playlist = enrichedPlaylist;
-    svc.setMetadata();
-    console.log('svc.metadata', svc.metadata);
-    return svc.playlist;
-  };
-
-  svc.isMaxSizeReached = function(playlist) {
-    return playlist[playlist.length - 1].startTime >= svc.playlistEnd;
-  };
-
-  svc.plRange = function(playlist) {
-    if(playlist.length > 0) {
-      let plEnd = secondsToHours(plSecondEnd(playlist));
-      return `${playlist[0].literalStart} - ${plEnd}`
-    }
-    return '--';
-  }
-
-  svc.availableSpace = function(pl) {
-    let playlist = pl ? pl : svc.playlist;
-    let availableSeconds;
-    if(playlist.length > 0) 
-      availableSeconds = (svc.playlistEnd - svc.playlistStart) - (plSecondEnd(playlist) - playlist[0].startTime);
-    else
-      availableSeconds = (svc.playlistEnd - svc.playlistStart);
-
-    return {
-      seconds: availableSeconds,
-      literal: secondsToHours(availableSeconds)
-    }
-  }
-
-  svc.plRate = function(playlist) {
-    console.log('in svc', playlist);
-    if(playlist.length > 0) {
-      let toto = Math.min(100, Math.round( 100 * (plSecondEnd(playlist) - playlist[0].startTime) / (svc.playlistEnd - svc.playlistStart) ));
-      return toto;
-    }
-    return 0;
-  }
-
-  svc.setMetadata = function() {
-    var plRate = svc.plRate(svc.playlist);
-    svc.metadata.values = { 
-      range : svc.plRange(svc.playlist), 
-      rate : plRate,
-      isMaxSizeReached: plRate >= 100
-    };
-  }
-
-  svc.secondsToHours = secondsToHours;
-
   return svc;
 }])
 
